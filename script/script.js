2804f1374fa6fda101a735a91c2df563653834cd
Kidist Dinku <95876178+Ney2@users.noreply.github.com>
2022-07-05 10:37:10 +0300

    Update script.js file

--- script/script.js
+++ script/script.js
@@ -13,43 +13,43 @@ class Book {
     author = this.author;
 }
 
- if (localStorage.getItem('bookList') === null) {
-   localStorage.setItem('bookList', []);
+if (localStorage.getItem('bookList') === null) {
+  localStorage.setItem('bookList', []);
 }
 
- if (localStorage.getItem('id') === null) {
-   localStorage.setItem('id', JSON.stringify(0));
+if (localStorage.getItem('id') === null) {
+  localStorage.setItem('id', JSON.stringify(0));
   id = JSON.parse(localStorage.getItem('id'));
- }
+}
 
 const refreshDOM = () => {
-   allBooks = JSON.parse(localStorage.getItem('bookList'));
- allBooks.forEach((book) => {
+  allBooks = JSON.parse(localStorage.getItem('bookList'));
+  allBooks.forEach((book) => {
     const bookTitle = book.name;
     const bookAuthor = book.author;
     const bookId = book.id;
     const removeBtn = document.createElement('button');
     removeBtn.innerText = 'Remove';
-     removeBtn.addEventListener('click', (e) => {
-     const { id } = e.target.parentNode;
+    removeBtn.addEventListener('click', (e) => {
+      const { id } = e.target.parentNode;
       allBooks = allBooks.filter((book) => book.id.toString() !== id.toString());
-      localStorage.setItem('bookList', JSON.stringify(allBooks));      // eslint-disable-next-line
+      localStorage.setItem('bookList', JSON.stringify(allBooks)); // eslint-disable-next-line
       location.reload();
     });
-     const newBook = document.createElement('div');
-     const newTitle = document.createElement('p');
-     const newAuthor = document.createElement('p');
-     const line = document.createElement('hr');
-     newTitle.innerText = bookTitle;
-     newAuthor.innerText = bookAuthor;
-     newBook.id = bookId;
-     newBook.appendChild(newTitle);
-     newBook.appendChild(newAuthor);
-     newBook.appendChild(removeBtn);
-     newBook.appendChild(line);
-     bookList.appendChild(newBook);
-   });
- };
+    const newBook = document.createElement('div');
+    const newTitle = document.createElement('p');
+    const newAuthor = document.createElement('p');
+    const line = document.createElement('hr');
+    newTitle.innerText = bookTitle;
+    newAuthor.innerText = bookAuthor;
+    newBook.id = bookId;
+    newBook.appendChild(newTitle);
+    newBook.appendChild(newAuthor);
+    newBook.appendChild(removeBtn);
+    newBook.appendChild(line);
+    bookList.appendChild(newBook);
+  });
+};
 
 window.onload = refreshDOM;
 button.addEventListener('click', (e) => {
